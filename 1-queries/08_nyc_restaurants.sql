7d7f751f720ddc2659c05cb454c60e47db27e06c
Sonny Li <sonnynomnom@gmail.com>
2024-06-29 14:00:50 -0400

    Update 08_nyc_restaurants.sql

--- 1-queries/08_nyc_restaurants.sql
+++ 1-queries/08_nyc_restaurants.sql
@@ -4,17 +4,37 @@
 SELECT *
 FROM restaurants;
 
+-- Unique restaurants
+
 SELECT DISTINCT cuisine
 FROM restaurants;
+
+-- Chinese spots
 
 SELECT *
 FROM restaurants
 WHERE cuisine = 'Chinese';
 
+-- Italian spots
+
 SELECT *
 FROM restaurants
 WHERE cuisine = 'Italian';
 
+-- Restaurants in Greenpoint
+
 SELECT *
 FROM restaurants
 WHERE neighborhood = 'Greenpoint';
+
+-- Cheat eats
+
+SELECT *
+FROM restaurants
+WHERE price = '$';
+
+-- Bougie spots
+
+SELECT *
+FROM restaurants
+WHERE price = '$$$';
