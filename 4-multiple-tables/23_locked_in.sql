efcc23cc947aaac300252c9fffe2540e6bc83cac
Sonny Li <sonnynomnom@gmail.com>
2025-05-29 14:39:31 -0400

    Update 23_locked_in.sql

--- 4-multiple-tables/23_locked_in.sql
+++ 4-multiple-tables/23_locked_in.sql
@@ -1,5 +1,4 @@
 -- Locked In 🔐
 -- Codédex
 
--- directors' id column is the primary key that's connected to 
--- books' author_id column is the foreign key that's connected to author's id column
+-- books' author_id column (foreign key) 🤝 author's id column (primary key)
